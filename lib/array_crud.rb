73ffc190b1cc68e5c37f194f9e0127b30dbd3ad5
Chyrelle Lewis <chyrellelewis@gmail.com>
2019-07-18 18:45:43 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -2,9 +2,7 @@ def create_an_empty_array
   []
 end
 
-def create_an_array = array.new
-  puts [house, dog, car, movie]
-  
+def create_an_array = array.new [house, dog, car, movie]
 end
 
 def add_element_to_end_of_array(array, element)
